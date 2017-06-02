b897c8af49916c39832ee5ed232e593b1daae85f
Dqicool <dqicool@outlook.com>
2017-06-02 15:44:07 +0800

    finish

--- Fmain.m
+++ Fmain.m
@@ -1,6 +1,6 @@
 function [Fx,Fy] = Fmain(X,Y,G,m,r0,ResMat)
     [Fxout, Fyout]  = Foutint(X, Y, G, m);
-    [Fxin , Fyin]   = Fin(X, Y, G, m, r0);
+    [Fxin , Fyin]   = Finnew(X, Y, G, m, r0);
     Fx              = (Fxin + Fxout) .* ResMat;
     Fx              = (abs(Fx) > 2.6121e-05) .* Fx;
     Fy              = (Fyin + Fyout) .* ResMat;
@@ -30,21 +30,6 @@ sizee =41;
     (log(p0 + (p0.^2 + t0.^2).^(1/2)) - log(p0 + (p0.^2 + t1.^2).^(1/2))));
 end
 
-function [Fxin , Fyin ] = Finnew(X, Y, G, m, r0)
-    sizee = 41;
-    Fxin    = zeros(sizee);
-    Fyin    = zeros(sizee); 
-    for ii0 = 1:sizee^2
-        for ii = ii0+1:sizee^2 
-            [dfx,dfy]   = dfff(X(ii0), Y(ii0), X(ii), Y(ii), r0, G, m(ii0), m(ii));
-            Fxin(ii0)   = dfx + Fxin(ii0);
-            Fyin(ii0)   = dfy + Fyin(ii0);
-            Fxin(ii)    = Fxin(ii) - dfx;
-            Fyin(ii)    = Fyin(ii) - dfy;
-        end
-    end
-end
-
 function [Fxin , Fyin ] = Fin(X, Y, G, m, r0)
 %对于体系内质点的影响 累加即可
     sizee = 41;
@@ -72,5 +57,31 @@ function [dfx,dfy] = dfff(X0, Y0, X, Y, r0)
     else
         dfx = 1000 * (X-X0);
         dfy = 1000 * (Y-Y0);
+    end
+end
+
+function [Fxin , Fyin ] = Finnew(X, Y, G, m, r0)
+    sizee = 41;
+    Fxin    = zeros(sizee);
+    Fyin    = zeros(sizee); 
+    for ii0 = 1:sizee^2
+        for ii = ii0+1:sizee^2 
+            [dfx,dfy]   = dffff(X(ii0), Y(ii0), X(ii), Y(ii), r0, G, m(ii0), m(ii));
+            Fxin(ii0)   = dfx + Fxin(ii0);
+            Fyin(ii0)   = dfy + Fyin(ii0);
+            Fxin(ii)    = Fxin(ii) - dfx;
+            Fyin(ii)    = Fyin(ii) - dfy;
+        end
+    end
+end
+
+function [dfx,dfy] = dffff(X0, Y0, X, Y, r0, G, m0, m)
+    rr  = (X - X0).^2 + (Y - Y0).^2;
+    if rr > r0^2
+        dfx = G .* m .* m0 .* (X - X0) / rr^(3/2);
+        dfy = G .* m .* m0 .* (Y - Y0) / rr^(3/2);
+    else
+        dfx = G * m0 * m * (X-X0) / r0^3;
+        dfy = G * m0 * m * (Y-Y0) / r0^3;
     end
 end
