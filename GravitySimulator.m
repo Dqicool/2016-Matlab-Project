892183d7d91bbba7a92f750973cb800a6c1dcec0
Dqicool <dqicool@outlook.com>
2017-06-03 10:23:43 +0800

    switch

--- GravitySimulator.m
+++ GravitySimulator.m
@@ -1,11 +1,10 @@
 cc
 Initial
-M = GravitySimulation(dt,G,r0,X,Y,sizee,m,vx,vy,ResMat);
+M = GravitySimulation(dt,G,r0,X,Y,sizee,m,vx,vy,ResMat,1);
 
-function M = GravitySimulation(dt,G,r0,X,Y,sizee,m,vx,vy,ResMat)
-    vidObj = VideoWriter('aaa.avi');
+function M = GravitySimulation(dt,G,r0,X,Y,sizee,m,vx,vy,ResMat,ode)
+    vidObj = VideoWriter('ddd.avi');
     open(vidObj);
-%   resMat = [zeros(5,sizee);zeros(sizee - 10,5),ones(sizee - 10),zeros(sizee - 10,5);zeros(5,sizee)];
     i = 0;
     while 1
         %第一次计算F1
@@ -16,27 +15,40 @@ function M = GravitySimulation(dt,G,r0,X,Y,sizee,m,vx,vy,ResMat)
         vy1             = vy + Fy1 .* dt ./ m;
         vx1             = vx1 .* ResMat;
         vy1             = vy1 .* ResMat;
-        X1             = X + vx.*dt;
-        Y1             = Y + vy.*dt;
-        [~, ~, X1, Y1]  = EdgeCheck(vx1,vy1,X1,Y1,sizee);
-        %利用第一次结果X1Y1计算第二次F2
-        [Fx2, Fy2]      = Fmain(X1,Y1,G,m,r0,ResMat);
-        %算出二阶龙格库塔F值
-        Fx              = (Fx1 + Fx2)./2;
-        Fy              = (Fy1 + Fy2)./2;
-        %F->R
-        vx              = vx + Fx .* dt ./ m;
-        vy              = vy + Fy .* dt ./ m;
-        [vx, vy]        = PosCheck(vx, vy, X, Y, r0, m);
-        vx              = vx .* ResMat;
-        vy              = vy .* ResMat;
-        X               = X + vx.*dt;
-        Y               = Y + vy.*dt;
-        [vx, vy, X, Y]  = EdgeCheck(vx,vy,X,Y,sizee);        
-        plot(X,Y,'r.');axis equal;axis([-15.5,15.5,-15.5,15.5]);
+        if ode == 1
+            [vx1, vy1]  = PosCheck(vx1, vy1, X, Y, r0, m);
+        end
+        X1              = X + vx.*dt;
+        Y1              = Y + vy.*dt;
+        [vx1, vy1, X1, Y1]  = EdgeCheck(vx1,vy1,X1,Y1,sizee);
+        if ode == 1
+            vx = vx1;
+            vy = vy1;
+            X  = X1;
+            Y  = Y1;
+        elseif ode == 2
+            %利用第一次结果X1Y1计算第二次F2
+            [Fx2, Fy2]      = Fmain(X1,Y1,G,m,r0,ResMat);
+            %算出二阶龙格库塔F值
+            Fx              = (Fx1 + Fx2)./2;
+            Fy              = (Fy1 + Fy2)./2;
+            %F->R
+            vx              = vx + Fx .* dt ./ m;
+            vy              = vy + Fy .* dt ./ m;
+            [vx, vy]        = PosCheck(vx, vy, X, Y, r0, m);
+            vx              = vx .* ResMat;
+            vy              = vy .* ResMat;
+            X               = X + vx.*dt;
+            Y               = Y + vy.*dt;
+            [vx, vy, X, Y]  = EdgeCheck(vx,vy,X,Y,sizee);        
+        end
+        plot(X,Y,'w.');axis equal;view(3);axis([-15.5,15.5,-15.5,15.5,-10,10]);set(gcf,'color',[0 0 0]/255);axis off
         i = i+1;
         disp(i);
-        M(i) = getframe;
-        writeVideo(vidObj,M(i));
+        if i ==1
+            pause(2);
+        end
+        M = getframe;
+        writeVideo(vidObj,M);
     end
 end
--- Initial.m
+++ Initial.m
@@ -13,9 +13,10 @@
     %vy(21,27) =  1;  
     rr     = X.^2 + Y.^2;
     ResMat = (rr<=225) .* (rr ~= 0);
+    %   resMat = [zeros(5,sizee);zeros(sizee - 10,5),ones(sizee - 10),zeros(sizee - 10,5);zeros(5,sizee)];
     for kkk = 1:sizee^2
         if rr(kkk) ~= 0
-            omega = 1.002 * (G .* m(21,21) ./ rr(kkk).^(3/2)).^(1/2);
+            omega = 1.005 * (G .* m(21,21) ./ rr(kkk).^(3/2)).^(1/2);
             vy(kkk) = omega .* X(kkk);
             vx(kkk) = omega .* -Y(kkk);
         end
